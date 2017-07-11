4b1ffe4dc0d7379098f2863c7b8d4d078373add0
thanosIrodotou <iraikky@gmail.com>
2017-07-11 17:30:46 +0100

    Quick fixes

--- src/greek_sites_crawler.py
+++ src/greek_sites_crawler.py
@@ -1,78 +1,78 @@
 import argparse
-
-from get_html import get_html
-from skai import skai
-from cnn import cnn
-from newsbomb import newsbomb
-from newsit import newsit
-from newsbeast import newsbeast
-from protothema import protothema
-from zougla import zougla
-from tovima import tovima
-
 from avgi import avgi
 from capital import capital
+from cnn import cnn
+from contra import contra
 from documentonews import documentonews
 from efsyn import efsyn
 from enikos import enikos
+from gazzetta import gazzetta
+from get_html import get_html
 from huffingtonpost import huffingtonpost
 from iefimerida import iefimerida
 from in_gr import in_gr
 from left import left
 from liberal import liberal
+from makeleio import makeleio
 from naftemporiki import naftemporiki
 from news247 import news247
+from newsbeast import newsbeast
+from newsbomb import newsbomb
+from newsit import newsit
 from nooz import nooz
-from  protagon import protagon
+from protagon import protagon
+from protothema import protothema
+from skai import skai
 from tanea import tanea
 from thetoc import thetoc
-from makeleio import makeleio
-from contra import contra
-from gazzetta import gazzetta
-import credits
+from tovima import tovima
+from zougla import zougla
+
+site = {}
+site['skai']=skai
+site['cnn']=cnn
+site['newsbomb']=newsbomb
+site['newsit'] = newsit
+site['newsbeast'] = newsbeast
+site['protothema']=protothema
+site['zougla']=zougla
+site['tovima']=tovima
+site['avgi']= avgi
+site['capital']=capital
+site['documentonews']=documentonews
+site['efsyn']=efsyn
+site['enikos']=enikos
+site['huffingtonpost']=huffingtonpost
+site['iefimerida']=iefimerida
+site['in_gr']=in_gr
+site['left']=left
+site['liberal']=liberal
+site['naftemporiki']=naftemporiki
+site['news247']=news247
+site['nooz']=nooz
+site['protagon']=protagon
+site['tanea']=tanea
+site['thetoc']=thetoc
+site['makeleio']=makeleio
+site['contra']=contra
+site['gazzetta']=gazzetta
+
 
 def greek_sites_crawler(url):
-	html = get_html(url)
-	site = {}
-	site['skai']=skai
-	site['cnn']=cnn
-	site['newsbomb']=newsbomb
-	site['newsit'] = newsit
-	site['newsbeast'] = newsbeast
-	site['protothema']=protothema
-	site['zougla']=zougla
-	site['tovima']=tovima
-	site['avgi']= avgi
-	site['capital']=capital
-	site['documentonews']=documentonews
-	site['efsyn']=efsyn
-	site['enikos']=enikos
-	site['huffingtonpost']=huffingtonpost
-	site['iefimerida']=iefimerida
-	site['in_gr']=in_gr
-	site['left']=left
-	site['liberal']=liberal
-	site['naftemporiki']=naftemporiki
-	site['news247']=news247
-	site['nooz']=nooz
-	site['protagon']=protagon
-	site['tanea']=tanea
-	site['thetoc']=thetoc
-	site['makeleio']=makeleio
-	site['contra']=contra
-	site['gazzetta']=gazzetta
+    html = get_html(url)
+    url = url.split('.')[1]
+    if url == 'in':
+        url = url+'_gr'
+    if site.get(url, None) == None:
+        print("This site dont provider yet \n")
+    else:
+        print( site[url](html))
+        return site[url](html)
 
-	url = 	url.split('.')[1]
-	if url =='in':
-		url = url+'_gr'
-	if site.get(url,None)==None:
-		print("This site dont provider yet \n")
-	else:
-		print( site[url](html))
-		return site[url](html)
 
-if __name__=='__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-url",help="Sites which can crawl:skai,cnn,newsbomb,newsit,newsbeast,protothema,zougla,t    ovima,avgi,capital,documentonews,efsyn,enikos,huffingtonpost,iefimerida,in_gr,left,liberal,naftemporiki,news247,noo    z,protagon,tanea,thetoc",type=str)
-	args = parser.parse_args()
-	greek_sites_crawler(args.url)
+if __name__ == '__main__':
+    helpString = 'Sites which we can crawl: {}'.format(','.join(site))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-url", help=helpString, type=str)
+    args = parser.parse_args()
+    greek_sites_crawler(args.url)
--- src/kathimerini.py
+++ src/kathimerini.py
@@ -1,6 +1,6 @@
 
 def kathimerini(html):
-	topic = .find("span",{"class":"item-category"}).text#html.title.text.split('|')[1].strip()
+	topic = html.find("span",{"class":"item-category"}).text#html.title.text.split('|')[1].strip()
 	title = html.title.text
 	article = html.find("div",{"class":"freetext"}).text
 	publish_time = html.find("time").text
