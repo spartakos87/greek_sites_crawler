bd5010b3e5d109071cb6db321f9a65cef7032833
serepasf <serepasf@yahoo.gr>
2017-07-13 23:24:07 +0300

    Fix bug of cnn.gr ,for the case where there isn't any specific topic

--- src/cnn.py
+++ src/cnn.py
@@ -1,10 +1,13 @@
 
 def cnn(html):
-	topic = str(html.find('ul',{ 'class':"nav-child submenu"})).split('href')
-	topic = [topic[k+1] for k,i in enumerate(topic) if 'active' in i][0]
-	topic = topic.split('>')[1].split('<')[0]
+	try:
+		topic = str(html.find('ul',{ 'class':"nav-child submenu"})).split('href')
+		topic = [topic[k+1] for k,i in enumerate(topic) if 'active' in i][0]
+		topic = topic.split('>')[1].split('<')[0]
+	except:
+		topic ='' # There isn't specific topic
 	title = html.title.text
-	article = html.find('div',{'class':"story-text story-intro"}).text
+	article = html.find('div',{'class':"story-text story-fulltext"}).text
 	publish_time = [i for i in html.find('div',{'class':"story-date story-credits icon icon-time"}).text.split('\t') if i!='' and i!='\n'][1]
 	return {'topic':topic,
 				'title':title,
