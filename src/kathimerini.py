1e36a5a440867bbc09a4c5ca8f66351349569e1e
serepasf <serepasf@yahoo.gr>
2017-07-13 17:16:12 +0300

    Fix bug to publish_time

--- src/kathimerini.py
+++ src/kathimerini.py
@@ -3,7 +3,7 @@ def kathimerini(html):
 	topic = html.find("span",{"class":"item-category"}).text#html.title.text.split('|')[1].strip()
 	title = html.title.text
 	article = html.find("div",{"class":"freetext"}).text
-	publish_time = html.find("time").text
+	publish_time = html.find("article",{"id":"item-article"}).find("time").text.split()[0]
 	return {'topic':topic,
 			'title':title,
 				'article':article,
