949d538124a116dde9d4dae0042ba859fbef7a61
serepasf <serepasf@yahoo.gr>
2017-07-13 23:16:36 +0300

    fix bug of date

--- src/parapolitika.py
+++ src/parapolitika.py
@@ -3,7 +3,7 @@ def parapolitika(html):
 	topic = html.find("span",{"class":"sect_text artSectText"}).text
 	title = html.title.text
 	article = html.find("div",{"class":"articleMainContentInner"}).text
-	publish_time = html.find("span",{"class":"pDate"}).text
+	publish_time = html.find("div",{"class":"articleHeader"}).find("span",{"class":"pDate"}).text.replace('\n','').strip().rstrip()
 	return {'topic':topic,
 			'title':title,
 				'article':article,
