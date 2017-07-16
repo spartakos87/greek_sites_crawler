d373d2949f9169752146538c464cbc8c9ab6abf5
serepasf <serepasf@yahoo.gr>
2017-07-16 15:30:43 +0300

    Crawler for neolaia.gr

---
+++ src/neolaia.py
@@ -0,0 +1,15 @@
+
+def neolaia(html):
+	try:
+		topic=html.find("li",{'class':"current"}).text
+	except:
+		topic=''
+	title=html.title.text
+	article = html.find{"div",{"class":"content"}).text
+	publish_time =str(html.find("p",{"class":"blog-author"})).split('</span>')[-1].replace('</p>','').strip()
+	return {'topic':topic,
+			'title':title,
+				'article':article,
+					'publish_time':publish_time
+		}
+
