d84675e3c909d60d4d5134f7e77c42f042fc255e
spartakos87 <serepasf@yahoo.gr>
2017-07-18 11:06:30 +0300

    fix bug

--- src/skai.py
+++ src/skai.py
@@ -4,7 +4,7 @@ def skai(html):
 	try:
 		topic = str(html.find("h3",{"class":"section-title"})).split('>')[1].split('<')[0]
 	except:
-		topic=''
+		topic= ' '
 	title = str(html.find('title')).replace('<title>','').replace('</title>','')
 	article = html.find('article').text
 	publish_time = re.findall(r'\d{2}/\d{2}/\d{4}',html.find("meta",{"name":'publish-date'}).text)[0]
