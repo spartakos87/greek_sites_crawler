3664b74b3b58e2d8c73c672ad84808aa87065c07
spartakos87 <serepasf@yahoo.gr>
2017-07-23 17:13:20 +0300

    Deleteed files

--- src/musicity.py
+++
@@ -1,9 +0,0 @@
-def musicity(html):
-	# There aren't topic and publish time
-	title = html.title.text
-	article = html.find("div",{"class":"itemFullText"}).text
-	return {'topic':'',
-			'title':title,
-				'article':article,
-					'publish_time':''
-		}
