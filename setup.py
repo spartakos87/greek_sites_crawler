ada3f746f4741d8e2c02192d8caf2445adde3a26
spartakos87 <serepasf@yahoo.gr>
2018-04-10 18:36:25 +0300

    fix bug

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ setup(
     version='1.0',
     packages=['greek_sites_crawler'],
     url='https://github.com/spartakos87/greek_sites_crawler',
+    download_url='https://github.com/spartakos87/greek_sites_crawler/archive/0.1.tar.gz',
     license='GPL-3.0',
     author='Serepas Filippas aka spartakos87',
     author_email='serepasf@yahoo.gr',
     description='Web crawler for plenty of greek sites',
+    keyword=['data','mining','crawler','webscrapping','scrape'],
     install_requires=['bs4']
 )
